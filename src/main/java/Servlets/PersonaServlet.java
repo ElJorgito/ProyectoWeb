b0fb80bba266869e64813951b02590eda44afafa
ElJorgito <11655449@alu.murciaeduca.es>
2025-03-24 15:54:10 +0100

    Commint Tercero

---
+++ src/main/java/Servlets/PersonaServlet.java
@@ -0,0 +1,37 @@
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import Model.persona;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/persona")
+public class PersonaServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Obtener parámetros de la URL
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        int edad = Integer.parseInt(request.getParameter("edad"));
+
+
+        // Crear objeto Persona
+        persona persona = new persona(nombre, apellido, edad);
+
+        // Respuesta HTML
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Datos de la Persona</h2>");
+        out.println("<p>Nombre: " + persona.getNombre() + "</p>");
+        out.println("<p>Apellido: " + persona.getApellido() + "</p>");
+        out.println("<p>Edad: " + persona.getEdad() + "</p>");
+        out.println("</body></html>");
+    }
+}
